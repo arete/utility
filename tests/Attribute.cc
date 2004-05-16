7fc7c031abc15f40b0a72cf05af08748b712b32f
rene <rene@7cc59f4a-5ebc-0310-b87a-ee8f54a180dd>
2004-05-16 20:37:45 +0000

    * fixed warning in preliminary Attribute code

--- tests/Attribute.cc
+++ tests/Attribute.cc
@@ -5,5 +5,8 @@ int main ()
 {
   GSMP::Attribute<int, true> a_with_signal;
   GSMP::Attribute<int, false> a_without_signal;
+  int x;
+  x = a_with_signal.Get ();
+  x = a_without_signal.Get ();
   return 0;
 }
