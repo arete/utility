668ea18069bb8f96956bf5c107ee0ccaca8cf25f
rene <rene@7cc59f4a-5ebc-0310-b87a-ee8f54a180dd>
2005-05-29 12:15:06 +0000

    * added the glog helper to our usual utility deposit

---
+++ include/Glob.hh
@@ -0,0 +1,44 @@
+
+#ifdef UTILITY__GLOB_HH__
+#define UTILITY__GLOB_HH__
+
+#include <glob.h>
+
+namespace Utility
+{
+
+/* very lightweight glob wrapper ... */
+class Glob
+{
+ public:
+
+  typedef char** iterator;
+
+  Glob (const std::string& pattern) {
+    glob (pattern.c_str(), 0, NULL, &g);
+  }
+  ~Glob () {
+    globfree (&g);
+  }
+
+  iterator begin() {
+    return g.gl_pathv;
+  }
+
+  iterator end() {
+    return g.gl_pathv + g.gl_pathc;
+  }
+  
+  operator void*() {
+    return g.gl_pathc == 0 ? 0 : this;
+  }
+  
+  bool operator!() const {
+    return g.gl_pathc == 0;
+  }
+  
+  private:
+    glob_t g;
+};
+
+#endif
