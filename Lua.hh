f2ecb458b5c9eb5df4046cdfdc7b93eb7b6b55be
rene <rene@7cc59f4a-5ebc-0310-b87a-ee8f54a180dd>
2009-08-22 11:40:27 +0000

    * added stackDump for convienience

--- Lua.hh
+++ Lua.hh
@@ -1048,6 +1048,23 @@ namespace LuaWrapper {
     lua_setfield(L, -2, name);
     lua_pop(L, 1);
   }
+
+  // convenient helper
+  inline void dumpStack (lua_State *L)
+  {
+    int top = lua_gettop(L);
+    for (int i = 1; i <= top; ++i)
+    {
+      int t = lua_type(L, i);
+      switch (t) {
+        case LUA_TSTRING:	printf("'%s' ", lua_tostring(L, i)); break;
+        case LUA_TBOOLEAN:	printf("%s ", lua_toboolean(L, i) ? "true" : "false"); break;
+        case LUA_TNUMBER:	printf("%g ", lua_tonumber(L, i)); break;
+        default:		printf("%s ", lua_typename(L, t)); break;
+      }
+    }
+    printf("\n");
+  }
 }
 
 
