af4dae5bf2a328b902a02e210a1444c53c82a8a4
rene <rene@7cc59f4a-5ebc-0310-b87a-ee8f54a180dd>
2011-07-18 13:23:01 +0000

    * bump copyright year, too

--- Lua.hh
+++ Lua.hh
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2008 Valentin Ziegler, ExactCODE GmbH
- * Copyright (C) 2009-2010 René Rebe, ExactCODE GmbH
+ * Copyright (C) 2009-2011 René Rebe, ExactCODE GmbH
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
